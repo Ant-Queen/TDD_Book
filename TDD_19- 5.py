efb1d711b6226d1dc4e7c82058d3955666fbf891
KSH <gorltl@naver.com>
2022-08-16 02:31:52 +0900

    19-5 추가

---
+++ TDD_19- 5.py
@@ -0,0 +1,32 @@
+class TestCase:
+    def __init__(self,name):
+        self.name=name
+    def run(self):
+        self.setUp()
+        method=getattr(self,self.name)
+        method()
+    def setUp(self):
+        pass
+
+class WasRun(TestCase):
+    def __init__(self,name):
+        self.wasRun=None
+        TestCase.__init__(self,name)
+    def testMethod(self):
+        self.wasRun=1
+    def setUp(self):
+        self.wasRun=None
+        self.wasSetUp=1
+
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test=WasRun("testMethod")
+        test.run()
+        assert(test.wasRun)
+    def testSetUp(self):
+        test=WasRun("testMethod")
+        test.run()
+        assert(test.wasSetUp)
+
+TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
