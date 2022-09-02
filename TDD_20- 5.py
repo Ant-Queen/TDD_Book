620f03d4274c91ca642e66fc4e03c5a6e575b323
KSH <gorltl@naver.com>
2022-09-02 13:21:35 +0900

    20-5 추가

---
+++ TDD_20- 5.py
@@ -0,0 +1,26 @@
+class TestCase:
+    def __init__(self,name):
+        self.name=name
+    def run(self):
+        self.setUp()
+        method=getattr(self,self.name)
+        method()
+    def setUp(self):
+        pass
+
+class WasRun(TestCase):
+    def testMethod(self):
+        self.wasRun=1
+        self.log=self.log+"testMethod "
+    def setUp(self):
+        self.wasRun=None
+        self.log="setUp "
+
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test=WasRun("testMethod")
+    def testTemplateMethod(self):
+        self.test.run()
+        assert("setUp testMethod "==self.test.log)
+
+TestCaseTest("testTemplateMethod").run()
